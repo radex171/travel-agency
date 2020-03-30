b6645a6eb6f1cc665e21f347777ba1e7257dfcaf
Radoslaw Zaborski <radex171@gmail.com>
2020-03-30 16:49:36 +0200

    add title options in single trip

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,21 +2,29 @@ import React from 'react';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
-const OrderForm = props => (
-
-  <Row>
-    <Col xs={12}>
-      <OrderSummary cost={props.tripCost} options={props.options}/>
-    </Col>
-  </Row>
-
-);
+const OrderForm = props => {
+  return (
+    <Row>
+      {pricing.map(option => (
+        <Col md={4} key={option.id}>
+          <OrderOption {...option} />
+        </Col>
+      ))}
+      
+      <Col xs={12}>
+        <OrderSummary cost={props.tripCost} options={props.options}/>
+      </Col>
+    </Row>
+  );
+};
 
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
+ 
 };
-
 
 export default OrderForm;
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
+
+const OrderOption = ({name}) => (
+  <div className={styles.component}>
+    <h3 className={styles.title}>{name}</h3>
+  </div>
+);
+OrderOption.propTypes = {
+  name: PropTypes.string,
+};
+export default OrderOption;
