310fe869d7f655bea4dd0675fcf44a5a16feffda
Radoslaw Zaborski <radex171@gmail.com>
2020-04-03 20:30:35 +0200

    add test in Hero & tripSummary

---
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import TripSummary from './TripSummary';
+
+describe ('component tripSummary', () =>{
+  it ('should render correct link id ', () =>{
+    const expectedId = 'abc';
+    const component = shallow(<TripSummary id={expectedId}/>);
+    expect (component.find('.link').prop('to')).toEqual(`/trip/${expectedId}`);
+  });
+
+  it ('should render correct src and alt', () => {
+    const expectedImageSrc = 'image.jpg';
+    const expectedImageAlt = 'not valible';
+    const component = shallow(<TripSummary src={expectedImageSrc} alt={expectedImageAlt}/>);
+
+    expect (component.find('img').prop('src').toEqual(expectedImageSrc));
+    expect(component.find('img').prop('alt')).toEqual(expectedImageAlt);
+    
+  });
+});
